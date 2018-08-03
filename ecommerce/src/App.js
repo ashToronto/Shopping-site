95e37f2c7b5403e7d6c3b5627e63e4b1010019b9
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-08-03 22:59:43 +0000

    added navigation

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Router from './Router'
+import { NavLink } from 'react-router-dom'
+
+const Navigation = () => <nav>
+<ul>
+<li><NavLink to='/'>Home</NavLink></li>
+<li><NavLink to='/cart'>Cart</NavLink></li>
+</ul>
+</nav>
 
 class App extends Component {
   render() {
     return <div>
       <h1>My Shopping Cart</h1>
-
+      <Navigation />
     <Router />
     </div>
   }
