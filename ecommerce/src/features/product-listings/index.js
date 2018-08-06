7b95d0cf505c75d2a24acec1017853f48244930e
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-08-06 16:29:35 +0000

    added css flex to arrange items

--- ecommerce/src/features/product-listings/index.js
+++ ecommerce/src/features/product-listings/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ProductListItem from './product-list-item';
 
 export default function productListing(props) {
-  return <div>
+  return <div className='product-listing'>
     {
     props.products.map(product =>
       <ProductListItem product = {product} />)
