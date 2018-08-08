53b041219ed356e64d5876978845227761d36886
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-08-08 23:27:25 +0000

    added some css to buttons

--- ecommerce/src/features/product-listings/add-button.js
+++ ecommerce/src/features/product-listings/add-button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function AddButton(props) {
-  return <button onClick = {() => props.addToCart(props.product)}>
+  return <button className='bttn-add' onClick = {() => props.addToCart(props.product)}>
     Add to cart ({
       (props.cartItem && props.cartItem.quantity) || 0
     })
