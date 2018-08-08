c2a384ddddb741075ed39fffadb28ff8d0a0d228
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-08-08 20:39:33 +0000

    adding helper functions

--- ecommerce/src/features/cart/reducer.js
+++ ecommerce/src/features/cart/reducer.js
@@ -1,3 +1,16 @@
+// Helper function to add all cart items without the new items id
+const cartWithoutItem = (cart, item) => cart.filter(cartItem => cartItem.id !== cartItem.id )
+
+//helper function to get a single item added to cart
+const itemInCart = (cart, item) => cart.filter(cartItem => cartItem.id === cartItem.id )[0]
+
+const addToCart = (cart, item) => {
+  const cartItem = itemInCart(cart, item)
+}
+
+
+
+// Setting state with reduce
 const cartReducer = (state=[], action) => {
   switch(action.type) {
     case 'ADD':
