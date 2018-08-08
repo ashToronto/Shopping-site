32f7ce121623d307cfb36907c47489825b1bbd12
ashwin <ashwin.ashwinkhanna@gmail.com>
2018-08-08 20:52:38 +0000

    refining code for cart

--- ecommerce/src/features/cart/reducer.js
+++ ecommerce/src/features/cart/reducer.js
@@ -6,6 +6,9 @@ const itemInCart = (cart, item) => cart.filter(cartItem => cartItem.id === cartI
 
 const addToCart = (cart, item) => {
   const cartItem = itemInCart(cart, item)
+  return cartItem === undefined
+  ? [...cartWithoutItem(cart, item), {...item, quantity: 1}]
+  ; [...cartWithoutItem(cart, item),(...cartItem, quantity: cartItem.quantity + 1 )]
 }
 
 
@@ -14,7 +17,7 @@ const addToCart = (cart, item) => {
 const cartReducer = (state=[], action) => {
   switch(action.type) {
     case 'ADD':
-    return [...state, action.payload]
+    return addToCart(state, action.payload)
 
     case 'REMOVE':
     //Removes only first instance of repeated items
